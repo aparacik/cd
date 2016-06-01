5d066df48a5c9dbf37c19e3566050d9649e17f58
Damian <dwrona94@gmail.com>
2016-06-01 19:25:33 +0100

    userdetailView

---
+++ application/views/account/detail.php
@@ -0,0 +1,21 @@
+		<div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <img src="http://placehold.it/380x500" alt="" class="img-rounded img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <h4>
+                            <?php echo
+								ucfirst($first_name) . ' ' .
+								ucfirst($last_name);
+							?>
+                         </h4>
+                        <small><i class="glyphicon glyphicon-pencil"></i><cite title="">Last login: <?php echo $last_login ?></cite></small>
+                        <p>
+                            <i class="glyphicon glyphicon-envelope"></i><?php echo $email ?>
+                            <br />
+                            <i class="glyphicon glyphicon-gift"></i>June 02, 1988</p>
+                        
+                    </div>
+                </div>
+        </div>
--- application/views/base_templates/base.php
+++ application/views/base_templates/base.php
@@ -57,13 +57,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
            <div class="dropdown">
           <a href="#" class="dropdown-toggle btn btn-danger" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'.$this->session->userdata('first_name').' <span class="caret"></span></a>
           <ul class="dropdown-menu">
+            <li><a href="' . base_url('account/detail') . '">Profil<span class="sr-only">(current)</span></a></li>
             <li><a href="' . base_url('games/add') . '">Dodawanie gier<span class="sr-only">(current)</span></a></li>
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href=" '.base_url().'accountin/logout">Logout</a></li>
+            <li><a href=" '.base_url().'account/logout">Logout</a></li>
           </ul>
         </div>
             
